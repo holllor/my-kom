549db7c5a340e4258576556e1d88af08244c62b8
holllor2@gmail.com <holllor2@gmail.com@524e2949-3ef4-916a-5f4b-5c2b7053aae1>
2013-02-13 17:28:47 +0000

    разобрался с авторизацией через контейнер томкат

--- Kom/src/java/ru/uvdt/kom/FaceKom.java
+++ Kom/src/java/ru/uvdt/kom/FaceKom.java
@@ -15,583 +15,583 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name = "FaceKom")
 @SessionScoped
 public class FaceKom implements Serializable {
-
+    
     public FaceKom() {
     }
-
+    
     public String[] getSpecZvan() {
         return specZvan;
     }
-
+    
     public void setSpecZvan(String[] specZvan) {
         this.specZvan = specZvan;
     }
-
+    
     public String getFace() {
         return face;
     }
-
+    
     public void setFace(String face) {
         this.face = face;
     }
-
+    
     public String[] getDolj1() {
         return dolj1;
     }
-
+    
     public void setDolj1(String[] dolj1) {
         this.dolj1 = dolj1;
     }
-
+    
     public String getFio1() {
         return fio1;
     }
-
+    
     public void setFio1(String fio1) {
+        System.out.println("set est");
         this.fio1 = fio1;
     }
-
+    
     public String getFio2() {
         return fio2;
     }
-
+    
     public void setFio2(String fio2) {
         this.fio2 = fio2;
     }
-
+    
     public String getFio3() {
         return fio3;
     }
-
+    
     public void setFio3(String fio3) {
         this.fio3 = fio3;
     }
-
+    
     public String getFormaK1() {
         return formaK1;
     }
-
+    
     public void setFormaK1(String formaK1) {
         this.formaK1 = formaK1;
     }
-
+    
     public String getFormaK2() {
         return formaK2;
     }
-
+    
     public void setFormaK2(String formaK2) {
         this.formaK2 = formaK2;
     }
-
+    
     public String getMestoK1() {
         return mestoK1;
     }
-
+    
     public void setMestoK1(String mestoK1) {
         this.mestoK1 = mestoK1;
     }
-
+    
     public String getNomerKu1() {
         return nomerKu1;
     }
-
+    
     public void setNomerKu1(String nomerKu1) {
         this.nomerKu1 = nomerKu1;
     }
-
+    
     public String getOsnovV1() {
         return osnovV1;
     }
-
+    
     public void setOsnovV1(String osnovV1) {
         this.osnovV1 = osnovV1;
     }
-
+    
     public String[] getOsnovV10() {
         return osnovV10;
     }
-
+    
     public void setOsnovV10(String[] osnovV10) {
         this.osnovV10 = osnovV10;
     }
-
+    
     public String[] getOsnovV11() {
         return osnovV11;
     }
-
+    
     public void setOsnovV11(String[] osnovV11) {
         this.osnovV11 = osnovV11;
     }
-
+    
     public String getOsnovV2() {
         return osnovV2;
     }
-
+    
     public void setOsnovV2(String osnovV2) {
         this.osnovV2 = osnovV2;
     }
-
+    
     public String getOsnovV3() {
         return osnovV3;
     }
-
+    
     public void setOsnovV3(String osnovV3) {
         this.osnovV3 = osnovV3;
     }
-
+    
     public String getOsnovV4() {
         return osnovV4;
     }
-
+    
     public void setOsnovV4(String osnovV4) {
         this.osnovV4 = osnovV4;
     }
-
+    
     public String[] getOsnovV5() {
         return osnovV5;
     }
-
+    
     public void setOsnovV5(String[] osnovV5) {
         this.osnovV5 = osnovV5;
     }
-
+    
     public String[] getOsnovV6() {
         return osnovV6;
     }
-
+    
     public void setOsnovV6(String[] osnovV6) {
         this.osnovV6 = osnovV6;
     }
-
+    
     public String getOsnovV7() {
         return osnovV7;
     }
-
+    
     public void setOsnovV7(String osnovV7) {
         this.osnovV7 = osnovV7;
     }
-
+    
     public String getOsnovV8() {
         return osnovV8;
     }
-
+    
     public void setOsnovV8(String osnovV8) {
         this.osnovV8 = osnovV8;
     }
-
+    
     public String getOsnovV9() {
         return osnovV9;
     }
-
+    
     public void setOsnovV9(String osnovV9) {
         this.osnovV9 = osnovV9;
     }
-
+    
     public String[] getPodraz1() {
         return podraz1;
     }
-
+    
     public void setPodraz1(String[] podraz1) {
         this.podraz1 = podraz1;
     }
-
+    
     public String[] getRukovG1() {
         return rukovG1;
     }
-
+    
     public void setRukovG1(String[] rukovG1) {
         this.rukovG1 = rukovG1;
     }
-
+    
     public String[] getRukovG2() {
         return rukovG2;
     }
-
+    
     public void setRukovG2(String[] rukovG2) {
         this.rukovG2 = rukovG2;
     }
-
+    
     public String getRukovG3() {
         return rukovG3;
     }
-
+    
     public void setRukovG3(String rukovG3) {
         this.rukovG3 = rukovG3;
     }
-
+    
     public String getRukovG4() {
         return rukovG4;
     }
-
+    
     public void setRukovG4(String rukovG4) {
         this.rukovG4 = rukovG4;
     }
-
+    
     public String getRukovG5() {
         return rukovG5;
     }
-
+    
     public void setRukovG5(String rukovG5) {
         this.rukovG5 = rukovG5;
     }
-
+    
     public String[] getSostavG1() {
         return sostavG1;
     }
-
+    
     public void setSostavG1(String[] sostavG1) {
         this.sostavG1 = sostavG1;
     }
-
+    
     public String[] getSpecZvn2() {
         return specZvn2;
     }
-
+    
     public void setSpecZvn2(String[] specZvn2) {
         this.specZvn2 = specZvn2;
     }
-
+    
     public String[] getSpecZvn3() {
         return specZvn3;
     }
-
+    
     public void setSpecZvn3(String[] specZvn3) {
         this.specZvn3 = specZvn3;
     }
-
+    
     public String getZadch1() {
         return zadch1;
     }
-
+    
     public void setZadch1(String zadch1) {
         this.zadch1 = zadch1;
     }
-
+    
     public String getZadch2() {
         return zadch2;
     }
-
+    
     public void setZadch2(String zadch2) {
         this.zadch2 = zadch2;
     }
-
+    
     public String getZadch3() {
         return zadch3;
     }
-
+    
     public void setZadch3(String zadch3) {
         this.zadch3 = zadch3;
     }
-
+    
     public String getSpecZvn1() {
         return specZvn1;
     }
-
+    
     public void setSpecZvn1(String specZvn1) {
         this.specZvn1 = specZvn1;
     }
-
+    
     public String getSostavG2() {
         return sostavG2;
     }
-
+    
     public void setSostavG2(String sostavG2) {
         this.sostavG2 = sostavG2;
     }
-
+    
     public String[] getSostavG3() {
         return sostavG3;
     }
-
+    
     public void setSostavG3(String[] sostavG3) {
         this.sostavG3 = sostavG3;
     }
-
+    
     public String getSostavG4() {
         return sostavG4;
     }
-
+    
     public void setSostavG4(String sostavG4) {
         this.sostavG4 = sostavG4;
     }
-
+    
     public String[] getSostavG5() {
         return sostavG5;
     }
-
+    
     public String getSrokK1() {
         return srokK1;
     }
-
+    
     public void setSrokK1(String srokK1) {
         this.srokK1 = srokK1;
     }
-
+    
     public String[] getSrokKprod1() {
         return srokKprod1;
     }
-
+    
     public void setSrokKprod1(String[] srokKprod1) {
         this.srokKprod1 = srokKprod1;
     }
-
+    
     public String[] getSrokKprod2() {
         return srokKprod2;
     }
-
+    
     public void setSrokKprod2(String[] srokKprod2) {
         this.srokKprod2 = srokKprod2;
     }
-
+    
     public String getSrokKprod3() {
         return srokKprod3;
     }
-
+    
     public void setSrokKprod3(String srokKprod3) {
         this.srokKprod3 = srokKprod3;
     }
-
+    
     public String getSrokKprod4() {
         return srokKprod4;
     }
-
+    
     public void setSrokKprod4(String srokKprod4) {
         this.srokKprod4 = srokKprod4;
     }
-
+    
     public String getSrokKprod5() {
         return srokKprod5;
     }
-
+    
     public void setSrokKprod5(String srokKprod5) {
         this.srokKprod5 = srokKprod5;
     }
-
+    
     public void setSostavG5(String[] sostavG5) {
         this.sostavG5 = sostavG5;
     }
-
+    
     public String getDatPrib1() {
         return datPrib1;
     }
-
+    
     public void setDatPrib1(String datPrib1) {
         this.datPrib1 = datPrib1;
     }
-
+    
     public String getDatUb1() {
         return datUb1;
     }
-
+    
     public void setDatUb1(String datUb1) {
         this.datUb1 = datUb1;
     }
-
+    
     public String getFormRel1() {
         return formRel1;
     }
-
+    
     public void setFormRel1(String formRel1) {
         this.formRel1 = formRel1;
     }
-
+    
     public String[] getFormRel2() {
         return formRel2;
     }
-
+    
     public void setFormRel2(String[] formRel2) {
         this.formRel2 = formRel2;
     }
-
+    
     public String[] getKontrOsNed1() {
         return kontrOsNed1;
     }
-
+    
     public void setKontrOsNed1(String[] kontrOsNed1) {
         this.kontrOsNed1 = kontrOsNed1;
     }
-
+    
     public String[] getKontrOsNed2() {
         return kontrOsNed2;
     }
-
+    
     public void setKontrOsNed2(String[] kontrOsNed2) {
         this.kontrOsNed2 = kontrOsNed2;
     }
-
+    
     public String getKontrOsNed3() {
         return kontrOsNed3;
     }
-
+    
     public void setKontrOsNed3(String kontrOsNed3) {
         this.kontrOsNed3 = kontrOsNed3;
     }
-
+    
     public String getKontrOsNed4() {
         return kontrOsNed4;
     }
-
+    
     public void setKontrOsNed4(String kontrOsNed4) {
         this.kontrOsNed4 = kontrOsNed4;
     }
-
+    
     public String getKontrOsNed5() {
         return kontrOsNed5;
     }
-
+    
     public void setKontrOsNed5(String kontrOsNed5) {
         this.kontrOsNed5 = kontrOsNed5;
     }
-
+    
     public String[] getKontrOsNed6() {
         return kontrOsNed6;
     }
-
+    
     public void setKontrOsNed6(String[] kontrOsNed6) {
         this.kontrOsNed6 = kontrOsNed6;
     }
-
+    
     public String[] getKontrOsNed7() {
         return kontrOsNed7;
     }
-
+    
     public void setKontrOsNed7(String[] kontrOsNed7) {
         this.kontrOsNed7 = kontrOsNed7;
     }
-
+    
     public String getProdVd1() {
         return prodVd1;
     }
-
+    
     public void setProdVd1(String prodVd1) {
         this.prodVd1 = prodVd1;
     }
-
+    
     public String[] getViavlN1() {
         return viavlN1;
     }
-
+    
     public void setViavlN1(String[] viavlN1) {
         this.viavlN1 = viavlN1;
     }
-
+    
     public String getViavlN2() {
         return viavlN2;
     }
-
+    
     public void setViavlN2(String viavlN2) {
         this.viavlN2 = viavlN2;
     }
-
+    
     public String getDatPodp1() {
         return datPodp1;
     }
-
+    
     public void setDatPodp1(String datPodp1) {
         this.datPodp1 = datPodp1;
     }
-
+    
     public String getDatPodp2() {
         return datPodp2;
     }
-
+    
     public void setDatPodp2(String datPodp2) {
         this.datPodp2 = datPodp2;
     }
-
+    
     public String getDatSos1() {
         return datSos1;
     }
-
+    
     public void setDatSos1(String datSos1) {
         this.datSos1 = datSos1;
     }
-
+    
     public String[] getFormRel3() {
         return formRel3;
     }
-
+    
     public void setFormRel3(String[] formRel3) {
         this.formRel3 = formRel3;
     }
-
+    
     public String getFormRel4() {
         return formRel4;
     }
-
+    
     public void setFormRel4(String formRel4) {
         this.formRel4 = formRel4;
     }
-
+    
     public String[] getFormRel5() {
         return formRel5;
     }
-
+    
     public void setFormRel5(String[] formRel5) {
         this.formRel5 = formRel5;
     }
-
+    
     public String getFormRel6() {
         return formRel6;
     }
-
+    
     public void setFormRel6(String formRel6) {
         this.formRel6 = formRel6;
     }
-
+    
     public String[] getFormRel7() {
         return formRel7;
     }
-
+    
     public void setFormRel7(String[] formRel7) {
         this.formRel7 = formRel7;
     }
-
+    
     public String getFormRel8() {
         return formRel8;
     }
-
+    
     public void setFormRel8(String formRel8) {
         this.formRel8 = formRel8;
     }
-
+    
     public String getOtmOvip1() {
         return otmOvip1;
     }
-
+    
     public void setOtmOvip1(String otmOvip1) {
         this.otmOvip1 = otmOvip1;
     }
-
+    
     public String getOtmOvip2() {
         return otmOvip2;
     }
-
+    
     public void setOtmOvip2(String otmOvip2) {
         this.otmOvip2 = otmOvip2;
     }
-
+    
     public String[] getViavlPol1() {
         return viavlPol1;
     }
-
+    
     public void setViavlPol1(String[] viavlPol1) {
         this.viavlPol1 = viavlPol1;
     }
-
+    
     public String getViavlPol2() {
         return viavlPol2;
     }
-
+    
     public void setViavlPol2(String viavlPol2) {
         this.viavlPol2 = viavlPol2;
     }
-    
     /**
      * dafdadfasdf
      */
     public String face = "start";
-    public String[] specZvan = new String[]{"рядовой", "лейтенант", "генерал"};
+    public String[] specZvan = new String[]{"рядовой", "лейтенант", "генерал", "залупа"};
     public String fio1;
     public String fio2;
     public String fio3;
@@ -661,10 +661,11 @@ public class FaceKom implements Serializable {
     public String datSos1;
     public String datPodp1;
     public String datPodp2;
-
-    public void next() {
-
-        setFio2(getFio1());
-
+    
+    public String next() {
+       
+        setFio1(datPodp1);
+        System.out.println("fio1= "+fio1);
+        return "welcome to hell "+ datPodp1+"  pizda "+fio1;
     }
 }
